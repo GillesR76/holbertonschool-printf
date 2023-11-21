0ca9853c341f1bbe5949187e8bb703be23174b3b
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-21 01:48:35 -0800

    updating our _putchar.c file with the implemented code we have written

--- _printf.c
+++ _printf.c
@@ -1,2 +1,54 @@
 #include <stdarg.h>
 #include "main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: pointer to a character string
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0, j = 0;
+	va_list list;
+
+	print tab[4] = {
+		{"c", print_char},
+		{"%", print_percent},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+
+	while (format[j] != '\0')
+	{
+		if (format[j] != '%')
+			_putchar(format[j]);
+		else
+		{
+			j++;
+
+			if (format[j] == 'c')
+			{
+				tab[0].f(list);
+				count++;
+			}
+			else if (format[j] == 's')
+			{
+				tab[2].f(list);
+				count++;
+			}
+			else if (format[j] == '%')
+			{
+				tab[1].f(list);
+				count++;
+			}
+		}
+	j++;
+	}
+	va_end(list);
+	return (count);
+}
