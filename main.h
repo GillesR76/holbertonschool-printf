5ae7eaf42bb11cb1fcb6602bda13ed59009e459f
Gilles Richard <gilles-richard76@hotmail.com>
2023-11-21 01:39:58 -0800

    updating main.h with the implemented prototype functions that we are calling within the _printf.c funtion

--- main.h
+++ main.h
@@ -6,5 +6,15 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+int _printf(const char *format, ...);
+void print_str(va_list list);
+void print_char(va_list list);
+void print_percent(va_list list);
+	
+typedef struct print
+{
+	char *caract;
+	void (*f)(va_list list);
+} print;
 
 #endif
